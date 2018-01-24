42be7e65a44e2b5c17e57570539d837ae4745e1d
Eden Mesfun <emesfun@gmail.com>
2018-01-24 15:09:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   return array.join(" and ") if array.length <= 2 #correct
   return array[0..1].join(", ") + ", and " + array[2] if array.length == 3 #correct
-  return array[0..4].join(", ") + ", and " + array.last if array.length > 3
+  return array[0..3].join(", ") + ", and " + array.last if array.length > 3
 end
 
 
