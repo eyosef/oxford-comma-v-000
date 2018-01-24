73942de7dbb459148e1485c8ece4fe32cc74d294
Eden Mesfun <emesfun@gmail.com>
2018-01-24 15:11:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
-def oxford_comma(array)
-  return array.join(" and ") if array.length <= 2 #correct
-  return array[0..1].join(", ") + ", and " + array[2] if array.length == 3 #correct
-  return array[0..3].join(", ") + ", and " + array.last if array.length > 3 #fix
-end
+# def oxford_comma(array)
+#   return array.join(" and ") if array.length <= 2 #correct
+#   return array[0..1].join(", ") + ", and " + array[2] if array.length == 3 #correct
+#   return array[0..3].join(", ") + ", and " + array.last if array.length > 3 #fix
+# end
 
 def oxford_comma(array)
   return array.join(" and ") if array.length <= 2 #correct
@@ -10,6 +10,9 @@ def oxford_comma(array)
   return array[0..3].join(", ") + ", and " + array.last if array.length > 3 #fix
 end
 
+users.except(myself).each do |user|
+  user.some_method
+end
 
 # return array[0..1].join(", ")
 # array[0..-2].join(", ") + " and " + array[-1]
