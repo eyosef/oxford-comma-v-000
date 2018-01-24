5f8c9d4848b22faa4334682e7572736fc201f13b
Eden Mesfun <emesfun@gmail.com>
2018-01-24 14:56:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ end
 
 # return array[0..1].join(", ")
 # array[0..-2].join(", ") + " and " + array[-1]
-#   return array[0..1].join(", ") + return array[2].join(" and ")if array.length > 2
+# return array[0..1].join(", ") + return array[2].join(" and ")if array.length > 2
 
 # :-]
